99ece86915b3a3c9bf2fb282ce4e77570df0cbb9
Radu Fritea <radufritea@gmail.com>
2022-04-24 09:03:54 +0300

    change settings for deployment

--- registru/registru/settings.py
+++ registru/registru/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f&ubl#o=+!koni%$ktp%)pp+dtu&nisu146jw%t7ul@&u3=(g7'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-f&ubl#o=+!koni%$ktp%)pp+dtu&nisu146jw%t7ul@&u3=(g7')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
