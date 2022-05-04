917a2dc6f413de9b452f29c5cb20a886b6be5692
Radu Fritea <radufritea@gmail.com>
2022-05-04 21:16:41 +0300

    final changes for serving static files et al

--- registru/settings.py
+++ registru/settings.py
@@ -15,7 +15,7 @@ import os
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -27,7 +27,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['92.82.64.58', '127.0.0.1', '.arovit.ro']
+ALLOWED_HOSTS = ['92.82.64.58', '127.0.0.1', 'dev.arovit.ro']
 
 AUTH_USER_MODEL = 'users.User'
 
@@ -135,11 +135,8 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '/')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+STATIC_ROOT = BASE_DIR / "static"
+STATIC_URL = "static"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
