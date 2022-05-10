6ecb0b8c44b20e1f2df58e4c2a5042296780bef2
Radu Fritea <radufritea@gmail.com>
2022-05-10 10:13:28 +0300

    spaces in sales.views

--- sales/views.py
+++ sales/views.py
@@ -13,138 +13,138 @@ week_day = my_date.weekday()
 
 def index(request):
 # WEEKLY PLANNING
-	pk = request.GET.get('pk')
-	start_date = my_date - timedelta(days=week_day)
-	end_date = my_date + timedelta(days=(4 - week_day))
-	plan = WeekPlan.objects.order_by('start_date').last()
-	week_plans = WeekPlan.objects.all().order_by('-start_date')
-	
-	visits = Visit.objects.all().order_by('-date_created')
-	paginator = Paginator(visits, 10)
-	page_number = request.GET.get('page')
-	page_obj = paginator.get_page(page_number)
-	
-	if request.method == "GET":
-	# first acces, no plans in db
-		if not week_plans:
-			form=PlanForm(initial={'start_date': start_date, 'end_date': end_date})
-		else:
-			if pk != None:
-				plan = WeekPlan.objects.get(id=pk)
-			else:
-				plan = WeekPlan.objects.get(start_date=start_date)	
-			
-			form = PlanForm(instance=plan)
+    pk = request.GET.get('pk')
+    start_date = my_date - timedelta(days=week_day)
+    end_date = my_date + timedelta(days=(4 - week_day))
+    plan = WeekPlan.objects.order_by('start_date').last()
+    week_plans = WeekPlan.objects.all().order_by('-start_date')
+    
+    visits = Visit.objects.all().order_by('-date_created')
+    paginator = Paginator(visits, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    
+    if request.method == "GET":
+    # first acces, no plans in db
+        if not week_plans:
+            form=PlanForm(initial={'start_date': start_date, 'end_date': end_date})
+        else:
+            if pk != None:
+                plan = WeekPlan.objects.get(id=pk)
+            else:
+                plan = WeekPlan.objects.get(start_date=start_date)	
+            
+            form = PlanForm(instance=plan)
 
-	elif request.method == 'POST':
-		form = PlanForm(request.POST, initial={'start_date': start_date, 'end_date': end_date})
-		if form.is_valid():
-			new_plan = form.save(commit=False)
-			agent = Agent.objects.get(user_id=request.user.id)
-			new_plan.agent_id = agent.id
-			new_plan.start_date = request.POST.get('start_date')
-			new_plan.end_date = request.POST.get('end_date')
-			if plan == None:
-				new_plan.id = 1
-			else:
-				new_plan.id = request.POST.get('plan_id')
-			new_plan.save()
-			print(new_plan.end_date)
-			return redirect(request.META['HTTP_REFERER'])
-		else:
-			print (form.errors)
-	
-	return render(request, 'sales/index.html', {
-		'form': form,
-		'plan': plan,
-		'page_obj': page_obj,
-		'week_plans': week_plans,
-	})
+    elif request.method == 'POST':
+        form = PlanForm(request.POST, initial={'start_date': start_date, 'end_date': end_date})
+        if form.is_valid():
+            new_plan = form.save(commit=False)
+            agent = Agent.objects.get(user_id=request.user.id)
+            new_plan.agent_id = agent.id
+            new_plan.start_date = request.POST.get('start_date')
+            new_plan.end_date = request.POST.get('end_date')
+            if plan == None:
+                new_plan.id = 1
+            else:
+                new_plan.id = request.POST.get('plan_id')
+            new_plan.save()
+            print(new_plan.end_date)
+            return redirect(request.META['HTTP_REFERER'])
+        else:
+            print (form.errors)
+    
+    return render(request, 'sales/index.html', {
+        'form': form,
+        'plan': plan,
+        'page_obj': page_obj,
+        'week_plans': week_plans,
+    })
 
 
 def add_plan(request):
-	if request.method == "POST":
-		form = PlanForm(data=request.POST)
-		if form.is_valid():
-			new_plan = form.save(commit=False)
-			agent = Agent.objects.get(user_id=request.user.id)
-			new_plan.agent_id = agent.id
-			new_plan.save()
-			return redirect('sales:index')
-	elif request.method == "GET":
-		form=PlanForm()
+    if request.method == "POST":
+        form = PlanForm(data=request.POST)
+        if form.is_valid():
+            new_plan = form.save(commit=False)
+            agent = Agent.objects.get(user_id=request.user.id)
+            new_plan.agent_id = agent.id
+            new_plan.save()
+            return redirect('sales:index')
+    elif request.method == "GET":
+        form=PlanForm()
 
-	return render(request, 'sales/add_plan.html', {'form': form})
+    return render(request, 'sales/add_plan.html', {'form': form})
 
 
 def plan(request, pk):
-	plan = WeekPlan.objects.get(id=pk)
-	form = PlanForm()
-	return render(request, 'sales/plan.html', {'plan': plan, 'form': form})
+    plan = WeekPlan.objects.get(id=pk)
+    form = PlanForm()
+    return render(request, 'sales/plan.html', {'plan': plan, 'form': form})
 
 
 def visits(request):
-	visits = Visit.objects.all().order_by('-date_created')
-	paginator = Paginator(visits, 10)
-	page_number = request.GET.get('page')
-	page_obj = paginator.get_page(page_number)
-	return render(request, 'sales/visits.html', {'page_obj': page_obj})
+    visits = Visit.objects.all().order_by('-date_created')
+    paginator = Paginator(visits, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'sales/visits.html', {'page_obj': page_obj})
 
 
 def visit(request, visit_id):
-	visit = Visit.objects.get(id=visit_id)
-	products = visit.products.all()
-	return render(request, 'sales/visit.html', {
-		'visit': visit,
-		'products': products
-		})
+    visit = Visit.objects.get(id=visit_id)
+    products = visit.products.all()
+    return render(request, 'sales/visit.html', {
+        'visit': visit,
+        'products': products
+        })
 
 
 def new_visit(request):
-	if request.method == "POST":
-		form = VisitForm(request.POST, request.FILES)
-		if form.is_valid():
-			agent = Agent.objects.get(user_id=request.user.id)
-			client = form.cleaned_data.get('client')
-			shop = form.cleaned_data.get('shop')
-			date_created = form.cleaned_data.get('date_created')
-			shelf_image = form.cleaned_data.get('shelf_image')
-			visit = Visit.objects.create(agent_id = agent.id, client_id = client.id, shop_id = shop.id, date_created = date_created, shelf_image = shelf_image)
-			products = form.cleaned_data.get('products')
-			for item in products:
-				product = Product.objects.get(id=item)
-				visit.products.add(product)
-			return HttpResponseRedirect(reverse('sales:visits'))
-		else:
-			print(form.errors)
-	
-	elif request.method == "GET":
-		form=VisitForm()
+    if request.method == "POST":
+        form = VisitForm(request.POST, request.FILES)
+        if form.is_valid():
+            agent = Agent.objects.get(user_id=request.user.id)
+            client = form.cleaned_data.get('client')
+            shop = form.cleaned_data.get('shop')
+            date_created = form.cleaned_data.get('date_created')
+            shelf_image = form.cleaned_data.get('shelf_image')
+            visit = Visit.objects.create(agent_id = agent.id, client_id = client.id, shop_id = shop.id, date_created = date_created, shelf_image = shelf_image)
+            products = form.cleaned_data.get('products')
+            for item in products:
+                product = Product.objects.get(id=item)
+                visit.products.add(product)
+            return HttpResponseRedirect(reverse('sales:visits'))
+        else:
+            print(form.errors)
+    
+    elif request.method == "GET":
+        form=VisitForm()
 
-	return render(request, 'sales/new_visit.html', {'form': form})
+    return render(request, 'sales/new_visit.html', {'form': form})
 
 
 def info_competition(request):
-	return render(request, 'sales/info_competition.html')
+    return render(request, 'sales/info_competition.html')
 
 def visits_reports(request):
-	agents = Agent.objects.all().order_by("user")
-	q = request.GET.get('q')
-	if q != None:
-		selected_agent = Agent.objects.get(id=q)
-	else:
-		selected_agent = None
+    agents = Agent.objects.all().order_by("user")
+    q = request.GET.get('q')
+    if q != None:
+        selected_agent = Agent.objects.get(id=q)
+    else:
+        selected_agent = None
 
-	if selected_agent == None:
-		visits = Visit.objects.all().order_by("-date_created")
-	else:
-		visits = Visit.objects.filter(agent=selected_agent)
+    if selected_agent == None:
+        visits = Visit.objects.all().order_by("-date_created")
+    else:
+        visits = Visit.objects.filter(agent=selected_agent)
 
-	return render(request, 'sales/visits_reports.html', {
-		'agents': agents,
-		'visits': visits,
-		'agent': selected_agent,
-	})
+    return render(request, 'sales/visits_reports.html', {
+        'agents': agents,
+        'visits': visits,
+        'agent': selected_agent,
+    })
 
 def competition_reports(request):
-	return render(request, 'sales/competition_reports.html')
+    return render(request, 'sales/competition_reports.html')
